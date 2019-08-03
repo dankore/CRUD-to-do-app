b6d87f627ecf1c465f2df83a6d4904e6eb51eeb8
adamu <zimmazone@yahoo.com>
2019-08-03 08:50:42 -0500

    Edit items part 2

--- public/browser.js
+++ public/browser.js
@@ -1,10 +1,25 @@
 document.addEventListener("click", e => {
   if (e.target.classList.contains("edit-me")) {
-    let userInput = prompt("enter");
-    axios.post('/update-item', {text: userInput}).then(()=>{
-        // Do something later
-    }).catch(()=>{
-        console.log('Please try again later');
-    })
+    let userInput = prompt(
+      "Edit your item",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    //If statement ensures when prompt is canceled, the item wont be updated to an empty string
+    if (userInput) {
+      axios
+        .post("/update-item", {
+          text: userInput,
+          id: e.target.getAttribute("data-id")
+        })
+        .then(() => {
+          //Run when axios action  is complete
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch(() => {
+          console.log("Please try again later");
+        });
+    }
   }
 });
--- server.js
+++ server.js
@@ -60,7 +60,9 @@ app.get("/", function(req, res) {
           return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
         <span class="item-text">${item.text}</span>
         <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+          <button data-id="${
+            item._id
+          }" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
           <button class="delete-me btn btn-danger btn-sm">Delete</button>
         </div>
       </li>`;
@@ -82,7 +84,12 @@ app.post("/create-item", function(req, res) {
   });
 });
 
-app.post('/update-item', (req, res)=>{
-    console.log(req.body.text);
-    res.send("Success")
-})
+app.post("/update-item", (req, res) => {
+  db.collection("items").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(req.body.id) },
+    { $set: { text: req.body.text } },
+    () => {
+      res.send("Success");
+    }
+  );
+});
